9f2a74e76517f2fa1f27ba667f1bafdcd02c6c13
Efe Öge <efeoge@gmail.com>
2020-03-30 12:45:28 +0300

    InclusiveDateTimeTZRange is added.

--- inclusive_django_range_fields/ranges.py
+++ inclusive_django_range_fields/ranges.py
@@ -1,4 +1,4 @@
-from psycopg2.extras import NumericRange, DateRange
+from psycopg2.extras import NumericRange, DateRange, DateTimeTZRange
 
 
 class InclusiveNumericRange(NumericRange):
@@ -8,6 +8,12 @@ class InclusiveNumericRange(NumericRange):
 
 
 class InclusiveDateRange(DateRange):
+
+    def __init__(self, lower=None, upper=None, bounds='[]', empty=False):
+        super().__init__(lower, upper, bounds, empty)
+
+
+class InclusiveDateTimeTZRange(DateTimeTZRange):
 
     def __init__(self, lower=None, upper=None, bounds='[]', empty=False):
         super().__init__(lower, upper, bounds, empty)
