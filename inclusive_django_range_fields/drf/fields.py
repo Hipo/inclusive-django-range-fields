a3c8be4116a03659356edb3e2a04ded251402e7e
Efe Öge <efeoge@gmail.com>
2020-05-21 14:59:08 +0300

    DRF validations are enhanced.

--- inclusive_django_range_fields/drf/fields.py
+++ inclusive_django_range_fields/drf/fields.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from inclusive_django_range_fields import InclusiveNumericRange, InclusiveDateRange
+
+
+def _validate_data_shape(data):
+    if not data.get("lower"):
+        raise ValidationError("lower is required")
+    elif not data.get("upper"):
+        raise ValidationError("upper is required")
 
 
 class InclusiveIntegerRangeField(serializers.Field):
@@ -12,7 +20,12 @@ class InclusiveIntegerRangeField(serializers.Field):
         }
 
     def to_internal_value(self, data):
-        return InclusiveNumericRange(lower=data["lower"], upper=data["upper"])
+        _validate_data_shape(data=data)
+
+        lower = serializers.IntegerField().to_internal_value(data["lower"])
+        upper = serializers.IntegerField().to_internal_value(data["upper"])
+
+        return InclusiveNumericRange(lower=lower, upper=upper)
 
 
 class InclusiveDateRangeField(serializers.Field):
@@ -24,4 +37,9 @@ class InclusiveDateRangeField(serializers.Field):
         }
 
     def to_internal_value(self, data):
-        return InclusiveDateRange(lower=data["lower"], upper=data["upper"])
+        _validate_data_shape(data=data)
+
+        lower = serializers.DateField().to_internal_value(data["lower"])
+        upper = serializers.DateField().to_internal_value(data["upper"])
+
+        return InclusiveDateRange(lower=lower, upper=upper)
